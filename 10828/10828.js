ae0f779889f761f7fe3fd17b3b94ff7e7757986f
sineTlsl <limsieun0313@gmail.com>
2023-03-12 14:37:31 +0900

    BAEKJOON 10828번

---
+++ 10828/10828.js
@@ -0,0 +1,33 @@
+// 걸린시간 00:42:52
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+const [n, ...arr] = input;
+
+console.log(solution(arr));
+
+function solution (arr) {
+  let stack = [];
+  let result = [];
+
+  arr.forEach(el => {
+    const cmd = el.split(' ')[0];
+    if (cmd === 'push') {
+      const item = el.split(' ')[1];
+      stack.push(item);
+    } else if (cmd === 'pop') {
+      if (stack.length === 0) { result.push(-1); } 
+      else {
+        result.push(stack[stack.length - 1]);
+        stack.pop();
+      }
+    } else if (cmd === 'size') {
+      result.push(stack.length);
+    } else if (cmd === 'empty') {
+      stack.length === 0 ? result.push(1) : result.push(0);   
+    } else if (cmd === 'top') {
+      stack.length === 0 ? result.push(-1) : result.push(stack[stack.length - 1]);
+    }
+  });
+
+  return result.join('\n');
+}
