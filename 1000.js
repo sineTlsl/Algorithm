2616302beaa171caea60525f809e4dd5b453104f
sineTlsl <limsieun0313@gmail.com>
2023-03-12 14:47:17 +0900

    BAEKJOON 1000번

---
+++ 1000.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().split(" ").map(Number);
+
+console.log(solution(input));
+
+function solution(input) {
+  const A = input[0];
+  const B = input[1];
+
+  return A + B;
+}
