c4a82bcadf710ed78d9a8f8b446b0ee82c998db8
sineTlsl <limsieun0313@gmail.com>
2023-03-16 20:20:38 +0900

    BAEKJOON 27866번

---
+++ 27866.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+console.log(solution(input));
+
+function solution(input) {
+  let [str, idx] = input[0].split("\n");
+
+  return str[idx - 1];
+}
