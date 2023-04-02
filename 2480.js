492ffe51df5cc58c324fc243be4a95289272f2ce
sineTlsl <limsieun0313@gmail.com>
2023-04-02 13:13:47 +0900

    BAEKJOON 2480번

---
+++ 2480.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
+
+console.log(solution(input));
+
+function solution(input) {
+  let dice1 = input[0];
+  let dice2 = input[1];
+  let dice3 = input[2];
+
+  if (dice1 === dice2 && dice1 === dice3 && dice2 === dice3) {
+    return 10000 + dice1 * 1000;
+  } else if (dice1 === dice2 || dice1 === dice3 || dice2 === dice3) {
+    if (dice1 === dice2) {
+      return 1000 + dice1 * 100;
+    } else {
+      return 1000 + dice3 * 100;
+    }
+  } else {
+    return Math.max(dice1, dice2, dice3) * 100;
+  }
+}
