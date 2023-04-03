7f674a8cb0cd074868671fe30c4c959399467dd3
sineTlsl <limsieun0313@gmail.com>
2023-04-03 09:46:22 +0900

    BAEKJOON 9498번

--- 9498.js
+++ 9498.js
@@ -6,13 +6,13 @@ console.log(solution(input));
 function solution(input) {
   let score = parseInt(input);
 
-  if (score >= 90) {
+  if (90 <= score && score <= 100) {
     return "A";
-  } else if (score >= 80) {
+  } else if (80 <= score && score <= 89) {
     return "B";
-  } else if (score >= 70) {
+  } else if (70 <= score && score <= 79) {
     return "C";
-  } else if (score >= 60) {
+  } else if (60 <= score && score <= 69) {
     return "D";
   } else {
     return "F";
