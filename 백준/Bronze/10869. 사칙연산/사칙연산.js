66c78bb2d64a29caad7814639c2f128c782fc703
Sieun <97720335+sineTlsl@users.noreply.github.com>
2023-04-08 21:20:46 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split(" ").map(Number);
+
+console.log(solution(input));
+
+function solution(input) {
+  const a = input[0];
+  const b = input[1];
+  let result = [];
+
+  result = [a + b, a - b, a * b, Math.floor(a / b), a % b];
+
+  return result.join("\n");
+}
