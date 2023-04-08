f4db8d0bccb82ea42b9da5dc99632be6455c9807
sineTlsl <limsieun0313@gmail.com>
2023-04-08 21:12:52 +0900

    Remove: File Delete

--- 10926.js
+++
@@ -1,8 +0,0 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().trim();
-
-console.log(solution(input));
-
-function solution(input) {
-  return input + "??!";
-}
--- 2562.js
+++
@@ -1,7 +0,0 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n").map(Number);
-
-let maxNum = Math.max(...input);
-
-console.log(maxNum);
-console.log(input.indexOf(maxNum) + 1);
