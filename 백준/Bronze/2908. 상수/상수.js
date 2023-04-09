ec5895e5c091440e5d27c6d5ebf53dd15ed0bd1e
Sieun <97720335+sineTlsl@users.noreply.github.com>
2023-04-09 10:58:48 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split(" ");
 
-let A = Number(input[0].split("").reverse().join(""));
-let B = Number(input[1].split("").reverse().join(""));
+let A = input[0].split("").reverse().join("");
+let B = input[1].split("").reverse().join("");
 
-A > B ? console.log(A) : console.log(B);
+console.log(Math.max(Number(A), Number(B)));
