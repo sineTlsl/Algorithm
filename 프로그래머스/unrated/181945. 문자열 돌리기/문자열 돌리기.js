0e7ae0f1c41bf35571a10d7ef7dc7ec0f3e0c4ea
Sieun <97720335+sineTlsl@users.noreply.github.com>
2023-04-29 15:03:04 +0900

    [unrated] Title: 문자열 돌리기, Time: 44.48 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    for (let el of str) {
+        console.log(el);
+    }
+});
