dca776ec846dff4e1f3bd5af1b75598cbe5c0b65
Sieun <97720335+sineTlsl@users.noreply.github.com>
2023-10-07 18:53:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+    let sum = 0;
+    
+    for (let i = 0; i < absolutes.length; i++) {
+        signs[i] ? sum += absolutes[i] : sum -= absolutes[i]
+    }
+    
+    return sum;
+}
