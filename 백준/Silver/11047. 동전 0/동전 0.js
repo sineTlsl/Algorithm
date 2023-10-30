51afd1675c56be02d7ef75ea5e7ad5f81fa7a21e
Sieun <97720335+sineTlsl@users.noreply.github.com>
2023-10-30 18:48:48 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
-const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const file = process.platform === 'linux' ? './dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().split('\n');
 
-let [N, K] = input[0].split(' ').map(Number);
-const coinArr = [];
+let [n, k] = input[0].split(' ').map(Number);
+const coins = [];
 let cnt = 0;
 
-for (let i = N; i >= 1; i--) {
-  coinArr.push(Number(input[i]));
+for (let i = n; i >= 1; i--) {
+  coins.push(Number(input[i]));
 }
 
-for (let i = 0; i < N; i++) {
-  cnt += Math.floor(K / coinArr[i]);
-  K %= coinArr[i];
+for (let i = 0; i < n; i++) {
+  cnt += Math.floor(k / coins[i]);
+  k %= coins[i];
 
-  if (K === 0) break;
+  if (k === 0) break;
 }
 
 console.log(cnt);
