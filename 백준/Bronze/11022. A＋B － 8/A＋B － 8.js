d2d17f03a2049cd9de200d151fc75af594cc2f03
Sieun <97720335+sineTlsl@users.noreply.github.com>
2023-11-05 22:53:22 +0900

    [Bronze V] Title: A+B - 8, Time: 128 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const T = Number(input[0]);
+
+for (let i = 1; i <= T; i++) {
+  const [A, B] = input[i].split(' ').map(Number);
+
+  console.log(`Case #${i}: ${A} + ${B} = ${A + B}`);
+}
