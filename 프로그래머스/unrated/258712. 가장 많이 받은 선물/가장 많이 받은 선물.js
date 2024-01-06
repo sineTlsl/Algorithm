b420f88bd23676dedbc35d51c2faec08a2dc9559
Sieun <97720335+sineTlsl@users.noreply.github.com>
2024-01-06 21:10:54 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 3.16 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,52 @@
+function solution(friends, gifts) {
+    // 1. 두 사람 사이에서 선물을 많이 주고받은 사람이 상대한테 선물받음 (+1)
+    
+    // 2. 두 사람 사이에서 선물을 주고 받지 않거나, 주고받은 수가 같다면?
+    // 선물 지수가 더 큰 사람이 선물 지수가 더 작은 사람한테 선물받음 (+1)
+    // 선물 지수: 자신과 친구들에게 준 선물의 수 - 받은 선물의 수
+    
+    // 예시
+    // A <-> B
+    // A가 준 선물: 3, 받은 선물: 7    // A의 선물 지수: -7
+    // B가 준 선물: 3, 받은 선물: 2    // A의 선물 지수: 1
+    
+    // 리턴: 다음달에 가장 많은 선물을 받는 친구가 받을 선물의 수
+    
+    let result = 0;
+    const name = {};
+    
+    // 1. 이름에 따라 인덱스로 저장
+    for (let i = 0; i < friends.length; i++) {
+        name[friends[i]] = i;
+    }
+    
+    // 2. 선물 지수 배열 및 선물[준 사람, 받은 사람] 0으로 초기화
+    const giftCountArr = new Array(friends.length).fill(0);
+    const giftsArr = new Array(friends.length).fill(0).map(() => new Array(friends.length).fill(0));
+    
+    // 3. 선물 주고받은 현황 저장
+    gifts.forEach(gift => {
+        const [a, b] = gift.split(' ');
+        giftsArr[name[a]][name[b]]++;
+        giftCountArr[name[a]]++;
+        giftCountArr[name[b]]--;
+    });
+    
+    // 4. 조건
+    for (let i = 0; i < giftCountArr.length; i++) {
+        let cnt = 0;
+        
+        for (let j = 0; j < giftCountArr.length; j++) {
+            if (i === j) continue;
+            
+            if (giftsArr[j][i] < giftsArr[i][j] || 
+                (giftsArr[j][i] === giftsArr[i][j] && 
+                 giftCountArr[i] > giftCountArr[j]))  {
+                cnt++;
+            }
+        }
+        if (cnt > result) result = cnt;
+    }
+    
+    return result;
+}
