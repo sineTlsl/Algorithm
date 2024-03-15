e4490af372cb2e6c9c5cf8521c01725829dac46f
Sieun <97720335+sineTlsl@users.noreply.github.com>
2024-03-15 20:09:01 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const result = [];
+    
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 !== 0) {
+            result.push(i);
+        }
+    }
+    
+    return result;
+}
