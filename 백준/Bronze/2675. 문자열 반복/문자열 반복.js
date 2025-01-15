e4158b287936378f976e91c3375a6bfb869d1bf2
Sieun <97720335+sineTlsl@users.noreply.github.com>
2025-01-15 21:11:13 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -1,14 +1,13 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().split("\n");
 
-let testCase = Number(input[0]);
-
-for (let i = 1; i <= testCase; i++) {
-  let [r, s] = input[i].split(" ");
+const inputCnt = Number(input.shift());
+for (let i = 0; i < inputCnt; i++) {
+  const [R, S] = input[i].split(" ");
   let result = "";
 
-  for (let j = 0; j < s.length; j++) {
-    result += s.charAt(j).repeat(r);
+  for (let char of S) {
+    result += char.repeat(R);
   }
   console.log(result);
 }
